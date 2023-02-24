e7aa7efdff32cee660c31aadafeaea0eb3ae613a
JamesHutchison <JamesGHutchison@gmail.com>
2023-02-24 21:40:56 +0000

    Change default git config to streamline pulls

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# git config that should be default
+git config --global pull.rebase true
+git config --global fetch.prune true
+git config --global diff.colorMoved zebra
+git config --global rebase.autoStash true
+
 pip install poetry
 
 poetry config virtualenvs.in-project true
