1b9fc11327d3fb73f612c33644f41348c6bf8529
JamesHutchison <JamesGHutchison@gmail.com>
2023-05-02 18:09:58 +0000

    Substantially improve import performance by fetching minimal source and frame info

--- megamock/import_machinery.py
+++ megamock/import_machinery.py
@@ -3,7 +3,7 @@ import inspect
 import linecache
 import re
 import sys
-from types import ModuleType
+from types import FrameType, ModuleType
 from typing import Callable
 
 from megamock.import_references import References
@@ -16,9 +16,51 @@ skip_modules = {
     "asttokens",
 }
 
+pat = re.compile(r"^(\s*def\s)|(\s*async\s+def\s)|(.*(?<!\w)lambda(:|\s))|^(\s*@)")
+
+
+def findsource(object):
+    """
+    A streamlined version of inspect.findsource
+    """
+
+    file = inspect.getsourcefile(object)
+    if file:
+        # Invalidate cache if needed.
+        linecache.checkcache(file)
+    else:
+        file = inspect.getfile(object)
+        # Allow filenames in form of "<something>" to pass through.
+        # `doctest` monkeypatches `linecache` module to enable
+        # inspection, so let `linecache.getlines` to be called.
+        if not (file.startswith("<") and file.endswith(">")):
+            raise OSError("source code not available")
+
+    module = inspect.getmodule(object, file)
+    if module:
+        lines = linecache.getlines(file, module.__dict__)
+    else:
+        lines = linecache.getlines(file)
+    if not lines:
+        raise OSError("could not get source code")
+    return lines
+
+
+def _get_code_lines(frame: FrameType) -> tuple[list[str], int]:
+    """
+    Streamlined logic to get code lines from a frame
+    """
+    lines = findsource(frame)
+    lineno = frame.f_lineno
+    start = lineno - 1
+    start = max(0, min(start, len(lines) - 1))
+    code_lines = lines[start : start + 1]
+
+    return code_lines, lineno
+
 
 def _reconstruct_full_line(
-    frame: inspect.FrameInfo, getline: Callable = linecache.getline
+    frame: FrameType, getline: Callable = linecache.getline
 ) -> str:
     """
     Given an import frame, reconstruct the full import line, which
@@ -28,15 +70,16 @@ def _reconstruct_full_line(
     :param getline: Pytest uses linecache.getline to rewrite assertions, so providing
         getline as a parameter for testing purposes
     """
-    code_lines: list[str] | None = frame.code_context
+
+    code_lines, lineno = _get_code_lines(frame)
     if code_lines:
         # if this is a multiline import, reconstruct the full line
         # the absense of '(' or '\' indicates a single line import
         next_line = code_lines[0]
         # note: timit gives 0.078569 for this vs 0.134779 for a compiled regex
         if "(" in next_line or "\\" in next_line:
-            filename = frame.filename
-            linenum = frame.lineno + 1
+            filename = frame.f_code.co_filename
+            linenum = lineno + 1
 
             lines = [next_line]
             paren_count = next_line.count("(") - next_line.count(")")
@@ -69,12 +112,11 @@ def start_import_mod() -> None:
             and (names := args[3])
         ):
             stack = inspect.stack()
-            for frame in stack:
-                if frame.code_context is None:
+            for i in range(1, 5):
+                frame = sys._getframe(i)
+                if frame.f_code.co_name == "new_import":
                     continue
-                if frame.function == "new_import":
-                    continue
-                calling_module = inspect.getmodule(frame[0])
+                calling_module = inspect.getmodule(frame)
                 if calling_module:
                     break
             assert calling_module
@@ -82,7 +124,7 @@ def start_import_mod() -> None:
             for k in names:
                 if full_line and (
                     renamed_result := re.search(
-                        rf"\s*from \S+ import.*{k}\s+as\s+(\w+)", full_line, re.DOTALL
+                        r"\s*from \S+ import.*\s+as\s+(\w+)", full_line, re.DOTALL
                     )
                 ):
                     renamed_to = renamed_result.group(1)
